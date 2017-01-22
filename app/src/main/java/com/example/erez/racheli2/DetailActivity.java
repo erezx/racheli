97ec21c571efb416b02a4f75c2217cd61491430a
seangal <sean1815@gmail.com>
2017-01-23 01:58:03 +0200

    Added motion sensing to Song and Story to start track

--- app/src/main/java/com/example/erez/racheli2/DetailActivity.java
+++ app/src/main/java/com/example/erez/racheli2/DetailActivity.java
@@ -13,12 +13,16 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.gemsense.common.GemSensorsData;
 import com.gemsense.common.TapData;
 import com.gemsense.gemsdk.Gem;
 import com.gemsense.gemsdk.GemListener;
 import com.gemsense.gemsdk.GemManager;
 import com.gemsense.gemsdk.GemSDKUtilityApp;
 import com.gemsense.gemsdk.OnTapListener;
+import com.gemsense.gemsdk.algorithms.QuaternionProjector;
+
+import static java.lang.Math.abs;
 
 /**
  * Provides UI for the Detail page with Collapsing Toolbar.
@@ -32,6 +36,9 @@ public class DetailActivity extends AppCompatActivity {
     int[] tracks = new int[15];
     int currentTrack = 0;
     int p;
+    float acc[];
+    float quat[];
+    private QuaternionProjector qProjector;
 
     public static final String EXTRA_POSITION = "position";
 
@@ -114,17 +121,93 @@ public class DetailActivity extends AppCompatActivity {
         }
         String[] whitelist = GemSDKUtilityApp.getWhiteList(this);
         initGem(whitelist[0]);
+        gem.calibrateAzimuth();
+        gem.calibrateOrigin();
+        qProjector = new QuaternionProjector(200f);
     }
 
     private void initGem(String address) {
         //Get a gem
         gem = GemManager.getDefault().getGem(address, new GemListener() {
             @Override
+            public void onSensorsChanged(GemSensorsData data) {
+
+                float a[] = data.acceleration;
+                float q[] = data.quaternion;
+                acc = a;
+                quat = q;
+                float[] qSphere = qProjector.projectOnPlane(quat);
+                float s = (qSphere[1] + 200)/400 + 0.5f; //speed
+                float p = (qSphere[1] + 200)/400 + 0.5f; //pitch
+
+                Log.i("Quatation", "Speed " + s);
+
+                if (mPlayer.isPlaying()) {
+                    float speed = abs(s);
+                    float pitch = abs(p);
+                    //float pitch = abs(qSphere[1]);
+                    Log.i("p", "p" + p);
+                    if(speed>0.5 && speed<1.5 && p!=0) {
+
+                 //       mPlayer.setPlaybackParams(mPlayer.getPlaybackParams().setSpeed(speed));
+                 //       mPlayer.setPlaybackParams(mPlayer.getPlaybackParams().setPitch(speed));
+                    }
+
+                }
+                if(abs(s-1f)>0.4f) {
+
+                    //    if(abs((a[0]+a[1]+a[2])/3 - 1)>0.4f) {
+                    Log.i("Motion detected", "Threshold " + abs(s-1f));
+                    //Log.i("Acceleration detected", "Threshold " + abs(a[0]+a[1]+a[2])/3);
+
+                    mPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+
+                        @Override
+                        public void onCompletion(MediaPlayer arg0) {
+                            if (tracks[currentTrack] != R.raw.silence) {
+                                currentTrack++;
+                                Log.i("Track", "Value " + currentTrack);
+                                Log.i("Track", "Value " + tracks.length);
+                                mPlayer.stop();
+                                mPlayer.release();
+                                mPlayer = null;
+                                mPlayer = MediaPlayer.create(getApplicationContext(), tracks[currentTrack]);//Create MediaPlayer object with MP3 file under res/raw folder
+                                //  mPlayer.setOnCompletionListener(this);
+                                //     float speed = abs(acc[2]+1);
+                                //float pitch = 3f;
+                                //    mPlayer.setPlaybackParams(mPlayer.getPlaybackParams().setSpeed(speed));
+                                //mPlayer.setPlaybackParams(mPlayer.getPlaybackParams().setPitch(pitch));
+                                gem.calibrateAzimuth();
+                                gem.calibrateOrigin();
+
+
+                            } else {
+                                currentTrack = 0;
+                                mPlayer.stop();
+                                mPlayer.release();
+                                mPlayer = null;
+                                mPlayer = MediaPlayer.create(getApplicationContext(), tracks[currentTrack]);//Create MediaPlayer object with MP3 file under res/raw folder
+                                gem.calibrateAzimuth();
+                                gem.calibrateOrigin();
+
+                            }
+                        }
+
+
+                    });
+                    mPlayer.start();
+                }
+            }
+            @Override
             public void onErrorOccurred(int i) {
                 Toast.makeText(DetailActivity.this, "Can't find a gem", Toast.LENGTH_SHORT).show();
             }
 
         });
+
+
+
+
         gem.setTapListener(new OnTapListener() {
             @Override
             public void onTap(TapData tapData) {
